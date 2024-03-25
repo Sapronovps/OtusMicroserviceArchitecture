ea9455a03194d46432029918235695ff02dce3c3
Pavel Sapronov <sapronovps@cloud-team.ru>
2024-03-25 14:42:42 +0300

    Otus Microservice Architecture - Четвертое домашнее задание

--- src/HomeworkFour/otus-app/resources/views/users/index.blade.php
+++ src/HomeworkFour/otus-app/resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>CRUD пользователей </h2>
+                <h2>CRUD пользователей</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('users.create') }}"> Создать нового пользователя</a>
